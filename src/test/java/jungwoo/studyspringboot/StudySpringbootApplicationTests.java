8fa5369749210d5b055bf433c630dbb65ebd27eb
jungwoow <yeriel.390@gmail.com>
2025-02-04 22:04:23 +0900

    Initial commit

---
+++ src/test/java/jungwoo/studyspringboot/StudySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package jungwoo.studyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
