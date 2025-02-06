e9e836184c0d6f5026b8804aa5283ab594b3fd6b
jungwoow <yeriel.390@gmail.com>
2025-02-06 20:19:05 +0900

    feat: 테스트 코드 실패 케이스 삭제

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -12,14 +12,4 @@ public class JUnitTest {
 
         Assertions.assertEquals(sum, a + b); // JUnit에서 제공하는 검증 메서드인 assertEquals() 로 값이 같은지 확인
     }
-
-    @DisplayName("1 + 3은 4이다.")
-    @Test
-    public void jUnitFailed(){
-        int a = 1;
-        int b = 3;
-        int sum = 3;
-
-        Assertions.assertEquals(sum, a + b); // 실패 케이스, 케이스가 하나라도 실패하면 전체 테스트를 실패한것으로 표시
-    }
 }
